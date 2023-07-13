4095b617ff21b0f168b4628aabbf27c1540c5316
Yogi <ygcahyono@gmail.com>
2023-07-13 11:57:29 +0000

    Update params tidy (v1)

--- notebook/set_params.py
+++ notebook/set_params.py
@@ -1,6 +1,5 @@
 from math import ceil
 from pandas import DataFrame, read_csv, concat, to_datetime, merge
-import matplotlib as plt
 
 def count_train_test(train_df, test_df):
     master_df = DataFrame()
@@ -63,78 +62,6 @@ def func_garch_train_test_split(validation = False, threshold = 24):
     test_df = test_df[test_df.Asset.isin(used_assets)]
 
     return train_df, valid_df, test_df
-
-from math import ceil
-from pandas import DataFrame, read_csv, concat, to_datetime
-
-def count_train_test(train_df, test_df):
-    master_df = DataFrame()
-
-    assets = train_df.Asset.unique().tolist()
-
-    for _, asset in enumerate(assets): 
-        df_train = train_df[train_df.Asset == asset]
-        # df_valid = valid_df[valid_df.Asset == asset]
-        df_test = test_df[test_df.Asset == asset]
-
-        master_df.loc[_ , 'Asset'] = asset
-        master_df.loc[_ , 'Train Length'] = df_train.shape[0]
-        # master_df.loc[_ , 'Valid Length'] = df_valid.shape[0]
-        master_df.loc[_ , 'Test Length'] = df_test.shape[0]
-        master_df.loc[_ , 'Total Length'] = df_train.shape[0] + df_test.shape[0]
-
-    return master_df
-
-def min_data_threshold(df, threshold = 24):
-
-    return df[df['Total Length'] >= threshold]['Asset'].tolist()
-
-def compile_train_test_garch(train_df, test_df, sample = True, base_params = True):
-    '''
-    '''
-
-    mresults = pd.DataFrame()
-
-    if base_params:
-        q, p = 1,1
-    if sample:
-        assets = [8589934333]
-    else:
-        assets = train_df.Asset.unique().tolist()
-
-    for i, asset in enumerate(assets): 
-        
-        rolling_predictions = []
-        test_size = test_df[test_df['Asset'] == asset].shape[0]
-        
-        y_volatility = train_df[train_df['Asset'] == asset]['V^YZ']
-        y_volatility.append(test_df[test_df['Asset'] == asset]['V^YZ'])
-        y_test = y_volatility[-test_size:]
-        
-        for i in range(test_size):
-            # train data
-            y_train = y_volatility[:-(test_size-i)]
-            model = arch_model(y_train, p=p, q=q)
-            model_fit = model.fit(disp='off')
-            # test data
-            pred = model_fit.forecast(horizon=1)
-            rolling_predictions.append(np.sqrt(pred.variance.values[-1,:][0]))
-
-        indices = y_volatility[-test_size:].index
-        rolling_predictions = pd.Series(rolling_predictions, index=indices)
-        
-        mse_million = mean_squared_error(y_test,pred_GARCH_rolling)*10**6
-        mresult = pd.DataFrame({
-            'Model':'GARCH(1,1)',
-            'Test Size': test_size,
-            'MSE^6':mse_million
-                    }
-            , index=[i]
-        )
-        mresults = pd.concat([mresults, mresult])
-
-    return mresults
-
 
 def func_train_test_split(validation = False, threshold = 24):
     '''
