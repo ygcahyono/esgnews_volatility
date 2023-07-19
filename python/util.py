bbe7788be3d5f66594e4ec89b8a416e551501a94
yogitricahyono <yogitricahyono@gmail.com>
2023-07-20 00:09:31 +0100

    Update Utils

--- python/util.py
+++ python/util.py
@@ -1,7 +1,17 @@
 from datetime import timedelta
-import pandas as pd
-import warnings
+from numpy import asarray, log1p, expm1
 from numpy import number
+from sklearn.metrics import mean_squared_error
+from statsmodels.regression.linear_model import OLS
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
+from math import ceil
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+import warnings
+
 warnings.filterwarnings('ignore')
 
 
@@ -10,6 +20,8 @@ class Data_Processing():
     Class to train and infer stock price for one particular stock
     '''
     def __init__(self, mt_start, mt_end, 
+                 validation = False,
+                 threshold = 24,
                  index_path = '../data/1.1-FTSE-IDX_VOL30-PRICES_2006-2023.csv',
                  price_path = '../data/1.1-FTSE_VOL30-PRICES_2006-2023.csv',
                  esg_path = '../data/1.2-FTSE_ESG_COR_2006-2023.csv'):
@@ -19,6 +31,30 @@ class Data_Processing():
         self.index_path = index_path
         self.price_path = price_path
         self.esg_path = esg_path
+        self.validation = validation
+        self.threshold = threshold
+
+    def count_train_test(train_df, test_df):
+        master_df = pd.DataFrame()
+
+        assets = train_df.Asset.unique().tolist()
+
+        for _, asset in enumerate(assets): 
+            df_train = train_df[train_df.Asset == asset]
+            # df_valid = valid_df[valid_df.Asset == asset]
+            df_test = test_df[test_df.Asset == asset]
+
+            master_df.loc[_ , 'Asset'] = asset
+            master_df.loc[_ , 'Train Length'] = df_train.shape[0]
+            # master_df.loc[_ , 'Valid Length'] = df_valid.shape[0]
+            master_df.loc[_ , 'Test Length'] = df_test.shape[0]
+            master_df.loc[_ , 'Total Length'] = df_train.shape[0] + df_test.shape[0]
+
+        return master_df
+    
+    def min_data_threshold(df, threshold = 24):
+
+        return df[df['Total Length'] >= threshold]['Asset'].tolist()
 
     def clean_count_missing_rows_assets(self, df):
         master_df = pd.DataFrame()
@@ -120,6 +156,58 @@ class Data_Processing():
 
         self.esg_df = esg_df
 
+    def func_train_test_split(self, validation = False, threshold = 24):
+        '''
+        '''
+        train_rows = .7
+        df = self.clean_df
+        df = df.rename(columns={'Date_x':'date_key'})
+        
+        df.date_key = pd.to_datetime(df.loc[:, 'date_key'])
+        df.month_key = pd.to_datetime(df.loc[:, 'month_key'])
+        df.index = df.month_key
+
+        train_df, valid_df, test_df = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+        asset_lists = df.Asset.unique()
+
+        for asset in asset_lists:
+            # subset dataframe
+            temp_df = df[df['Asset'] == asset].copy()
+
+            # parameters
+            rows = temp_df.shape[0]
+            train_len = ceil(rows*train_rows)
+
+            # setting up volatility lag to a dataframe
+            vol_df = pd.DataFrame({
+            'vol_series_daily' : temp_df['V^YZ'].shift(1),
+            'vol_series_weekly' : temp_df['V^YZ'].rolling(3).mean().shift(1),
+            'vol_series_monthly' : temp_df['V^YZ'].rolling(12).mean().shift(1)
+            })
+
+            temp_df = pd.merge(temp_df, vol_df, how = 'left', left_index=True, right_index=True)
+
+            # split the subset into train_df
+            train_df = pd.concat([temp_df.iloc[:train_len], train_df])
+            test_df = pd.concat([temp_df.iloc[train_len:], test_df])
+
+            if validation:
+                # if yes validation has 20% of the portion.
+                valid_len = int(rows*.2)
+
+                valid_df = pd.concat([temp_df.iloc[train_len:(train_len+valid_len)], valid_df])
+                valid_df = pd.concat([temp_df.iloc[(train_len+valid_len):], valid_df])
+
+        
+        master_df = self.count_train_test(train_df, test_df) # count the total rows of each assets
+        used_assets = self.min_data_threshold(master_df, threshold= threshold)     # filter out assets that has least data points
+
+        train_df = train_df[train_df.Asset.isin(used_assets)]
+        # valid_df = valid_df[valid_df.Asset.isin(used_assets)]
+        test_df = test_df[test_df.Asset.isin(used_assets)]
+
+        return train_df, valid_df, test_df
+
     def merge_data(self):
         '''
         '''
@@ -144,6 +232,9 @@ class Data_Processing():
         The excluded columns: ResourceUse, HumanRights, CSRStrategy, and Emissions were selected
         Based on columns that mostly contribute null to the FTSE assets.
         '''
+        validation = self.validation
+        threshold = self.threshold
+
         self.merge_data()
 
         #filter exclude columns
@@ -160,4 +251,167 @@ class Data_Processing():
             clean_df = self.clean_filna_assets_df(clean_df)
 
         self.clean_df = clean_df
-        return clean_df
+
+        train_df, valid_df, test_df = self.func_train_test_split(validation=validation, threshold=threshold)
+
+        return clean_df, train_df, valid_df, test_df
+
+
+class Run_Algorithms():
+    '''
+    '''
+
+
+    def __init__(self, train_df, test_df, algorithms,
+                 vif_factor = 5, sample = True, features = 'm1', outputs = False, cap = False):
+        
+        self.features = features
+        self.train_df = train_df
+        self.test_df = test_df
+        self.algorithms = algorithms 
+        self.vif_factor = vif_factor
+        self.outputs = outputs
+        self.cap = cap
+
+    def vif_check(self, feature_version = 2):
+
+        train_df = self.train_df
+        test_df = self.test_df
+        vif_factor = self.vif_factor
+        
+        merge_df = pd.concat([train_df, test_df])
+        merge_df = pd.merge_df.dropna()
+
+        cols = self.features_selections(features = 'm3', feature_version = feature_version)
+
+        merge_df = merge_df[cols]
+        merge_df = add_constant(merge_df)
+
+        vif = pd.DataFrame()
+        vif['VIF Factor'] = [variance_inflation_factor(merge_df.values, i) for i in range(merge_df.shape[1])]
+        vif['features'] = merge_df.columns
+        
+        features = vif[vif['VIF Factor'] <= vif_factor]['features'].tolist()
+        
+        return features
+
+    def asset_selections(self, sample = True):
+
+        assets = self.train_df.Asset.unique().tolist()
+        if sample: 
+            assets = [4295894970, 8589934212]
+
+        self.assets = assets
+
+    def features_selections(self, features = 'm1', feature_version = 2):
+        '''
+        Current version #2 is the version after pre-processing and removing four columns and does not including the variables
+        that come from HAR algorithms
+        '''
+
+        if features == 'm1':
+            cols = ['vol_series_daily', 'vol_series_weekly', 'vol_series_monthly', 'V^YZ']
+
+        elif features == 'm3':
+
+            # version 1
+            cols = [
+                'buzz','ESG','ESGCombined','ESGControversies','EnvironmentalPillar','GovernancePillar','SocialPillar'
+                            ,'CSRStrategy','Community','Emissions','EnvironmentalInnovation','HumanRights','Management','ProductResponsibility'
+                            ,'ResourceUse','Shareholders','Workforce', 'vol_series_daily','vol_series_weekly','vol_series_monthly', 'V^YZ']
+        
+            if feature_version == 2:
+                # version 2
+                cols = ['buzz','ESG','ESGCombined','ESGControversies','EnvironmentalPillar','GovernancePillar','SocialPillar','Community',
+                        'EnvironmentalInnovation','Management','ProductResponsibility','Shareholders','Workforce', 'V^YZ']
+        
+        else:
+            cols = self.vif_check(feature_version = feature_version)
+
+            self.cols = cols
+
+    def vis_line_plot_results(y_pred, y_test, algorithms, name = 'BARCLAYS', r = 1, features = 'm1'):
+
+        plt.figure(figsize=(10,4))
+        true, = plt.plot(y_test)
+        preds, = plt.plot(y_pred)
+        plt.title(f'{algorithms}-{features}-{name}', fontsize=15)
+        plt.legend(['True Volatility', 'Predicted Volatility'], fontsize=9)
+        plt.xticks(rotation=45)
+        plt.savefig(f'../outputs/{algorithms}-{features}/{str(r+1).zfill(3)}-{algorithms}-{name}.png')
+        plt.close()
+
+    def onetime_forward_validation(self, train_df, test_df, assets, cols, algorithms, cap = False):
+
+        outputs = self.outputs
+        features = self.features
+
+        if algorithms == 'OLS':
+            MODEL = OLS
+
+        for r, asset in enumerate(assets): 
+
+            name = train_df[train_df['Asset'] == asset].iloc[0,-1]
+
+            df_train = train_df[train_df.Asset == asset][cols].dropna()
+            df_test = test_df[test_df.Asset == asset][cols].dropna()
+            test_size = df_test.shape[0]
+            
+            df_train = log1p(df_train)
+            df_test = log1p(df_test)
+            
+            X_train = df_train.drop(['V^YZ'], axis=1)
+            X_test = df_test.drop(['V^YZ'], axis=1)
+            
+            y_train = df_train['V^YZ']
+            y_test = df_test['V^YZ']
+            
+            X_train = sm.add_constant(X_train)
+            X_test.loc[:, 'const'] = 1
+            X_test = X_test[X_train.columns]
+
+            # Fit the model
+            model = MODEL(y_train, X_train)
+            model_fit = model.fit()
+
+            # display(X_test, X_train)
+            y_pred = model_fit.predict(X_test)
+
+            if cap:
+                y_pred = y_pred.clip(lower = 0)
+            
+            mse_million = mean_squared_error(y_test,y_pred)*10**3
+            mresult = pd.DataFrame({
+                'Asset': asset,
+                'Name': name,
+                'Model': algo,
+                'Test Size': test_size,
+                'MSE^3':mse_million
+                        }
+                , index=[r]
+            )
+
+            mresults = pd.concat([mresults, mresult])
+            
+            y_test = expm1(y_test)
+            y_pred = expm1(y_pred)
+
+            if outputs: 
+                self.vis_line_plot_results(y_pred, y_test, model = 'HAR', features = features, name = name, r = r)
+
+        return mresults
+
+    def compile_train_test(self):
+        '''
+        '''
+
+        assets = self.assets
+        cols = self.cols
+        train_df = self.train_df
+        test_df = self.test_df
+        algorithms = self.algorithms
+        cap = self.cap
+
+        mresults = self.onetime_forward_validation(train_df, test_df, assets, cols, algorithms, cap)
+
+        return mresults
