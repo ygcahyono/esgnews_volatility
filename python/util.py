41defdc960a723ca3065957dad0b0fe0a4731968
yogitricahyono <yogitricahyono@gmail.com>
2023-07-20 01:55:26 +0100

    Update Code until GARCH

--- python/util.py
+++ python/util.py
@@ -1,3 +1,6 @@
+from arch import arch_model
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
 from datetime import timedelta
 from numpy import asarray, log1p, expm1
 from numpy import number
@@ -11,10 +14,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
 import warnings
+import numpy as np
 import sys
 
 warnings.filterwarnings('ignore')
 
+def calculate_iqr(values):
+    # Calculate Q1
+    Q1 = np.percentile(values, 25)
+    # Calculate Q3
+    Q3 = np.percentile(values, 75)
+    # Calculate IQR
+    IQR = Q3 - Q1
+    return IQR
+
+def detect_outliers_iqr(values):
+    # Calculate the IQR of the values
+    IQR = calculate_iqr(values)
+    # Calculate Q1 and Q3
+    Q1 = np.percentile(values, 25)
+    Q3 = np.percentile(values, 75)
+    # Define the lower and upper bound for outliers
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    # Return a boolean array: True if the value is an outlier, False otherwise
+    return lower_bound, upper_bound
 
 class Data_Processing():
     '''
@@ -355,7 +379,78 @@ class Run_Algorithms():
         plt.savefig(f'../outputs/{algorithms}-{features}/{str(r+1).zfill(3)}-{algorithms}-{name}.png')
         plt.close()
 
-    def onetime_forward_validation(self, train_df, test_df, cap = False):
+    def run_ols(self, train_df, test_df, asset, cols, cap = False, target = 'V^YZ'):
+
+        # (df_train, df_test, asset, target = 'V^YZ')
+        df_train = train_df[train_df.Asset == asset][cols].dropna()
+        df_test = test_df[test_df.Asset == asset][cols].dropna()
+        
+        df_train = log1p(df_train)
+        df_test = log1p(df_test)
+        
+        X_train = df_train.drop([target], axis=1)
+        X_test = df_test.drop([target], axis=1)
+        
+        y_train = df_train[target]
+        y_test = df_test[target]
+
+        # if algorithms == 'HAR':
+        # deactivate the if else of HAR
+        X_train = sm.add_constant(X_train)
+        X_test.loc[:, 'const'] = 1
+        X_test = X_test[X_train.columns]
+
+        # Fit the model
+        model = OLS(y_train, X_train)
+        model_fit = model.fit()
+
+        # display(X_test, X_train)
+        y_pred = model_fit.predict(X_test)
+
+        y_test = expm1(y_test)
+        y_pred = expm1(y_pred)
+
+        if cap:
+            y_pred = y_pred.clip(lower = 0)
+
+        return y_test, y_pred
+    
+    def run_garch(self, train_df, test_df, asset, cap = False
+                  , target = 'V^YZ'
+                  , p = 1
+                  , q = 1):
+        
+        y_pred = []
+        test_size = test_df[test_df['Asset'] == asset].shape[0]
+        
+        y_volatility = train_df[train_df['Asset'] == asset][target]
+        y_volatility.append(test_df[test_df['Asset'] == asset][target])
+        y_test = y_volatility[-test_size:]
+        
+        for i in range(test_size):
+            # train data
+            y_train = y_volatility[:-(test_size-i)]
+            model = arch_model(y_train, p=p, q=q)
+            model_fit = model.fit(disp='off')
+            # test data
+            pred = model_fit.forecast(horizon=1)
+            pred = np.sqrt(pred.variance.values[-1,:][0])
+            
+            # capping the outliers
+            if cap:
+                lower_bound, upper_bound = detect_outliers_iqr(y_train)
+                if upper_bound < pred:
+                    pred = upper_bound
+            
+            y_pred.append(pred)
+
+        indices = test_df[test_df.Asset == asset].index
+        y_pred = pd.Series(y_pred, index=indices)
+
+        return y_test, y_pred, test_size
+
+
+    def go_iterate_assets(self, train_df, test_df, cap = False):
 
         outputs = self.outputs
         features = self.features
@@ -371,35 +466,16 @@ class Run_Algorithms():
         for r, asset in enumerate(assets): 
             
             name = coverage_df[coverage_df.Asset == asset].iloc[0, 1]
-
-            df_train = train_df[train_df.Asset == asset][cols].dropna()
-            df_test = test_df[test_df.Asset == asset][cols].dropna()
-            test_size = df_test.shape[0]
             
-            df_train = log1p(df_train)
-            df_test = log1p(df_test)
-            
-            X_train = df_train.drop(['V^YZ'], axis=1)
-            X_test = df_test.drop(['V^YZ'], axis=1)
-            
-            y_train = df_train['V^YZ']
-            y_test = df_test['V^YZ']
 
-            # if algorithms == 'HAR':
-            # deactivate the if else of HAR
-            X_train = sm.add_constant(X_train)
-            X_test.loc[:, 'const'] = 1
-            X_test = X_test[X_train.columns]
+            if algorithms == 'HAR':
+                test_size = test_df.shape[0]
+                y_test, y_pred = self.run_ols(train_df, test_df, asset, cols)
 
-            # Fit the model
-            model = OLS(y_train, X_train)
-            model_fit = model.fit()
+            elif algorithms == 'GARCH':
+                # print('here')
+                y_test, y_pred, test_size = self.run_garch(train_df, test_df, asset)
 
-            # display(X_test, X_train)
-            y_pred = model_fit.predict(X_test)
-
-            if cap:
-                y_pred = y_pred.clip(lower = 0)
             
             mse_million = mean_squared_error(y_test,y_pred)*10**3
             mresult = pd.DataFrame({
@@ -414,9 +490,6 @@ class Run_Algorithms():
             )
 
             mresults = pd.concat([mresults, mresult])
-            
-            y_test = expm1(y_test)
-            y_pred = expm1(y_pred)
 
             if outputs: 
                 self.vis_line_plot_results(y_pred, y_test, model = 'HAR', features = features, name = name, r = r)
@@ -434,8 +507,7 @@ class Run_Algorithms():
         train_df.Asset = train_df.Asset.astype(int)
         test_df.Asset = test_df.Asset.astype(int)
 
-        if algorithms == 'HAR':
+        mresults = self.go_iterate_assets(train_df, test_df)
 
-            mresults = self.onetime_forward_validation(train_df, test_df)
 
         return mresults
